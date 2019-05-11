47fb279e80bb11ba1b13b4533e74cefac661cb7c
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2019-05-11 19:11:20 -0400

    Implemented puts function

--- CASM/src/main.asm
+++ CASM/src/main.asm
@@ -17,7 +17,7 @@
 
     SECTION .data
 
-str:    db "abcdef", 0
+str:    db "abcdef", 10, 0  ; Test string while I implement I/O functionality.
 
     SECTION .text
 
@@ -108,6 +108,20 @@ strlen:
 ;   library 'puts' function. Short for "put string," the function prints the
 ;   passed-in string to stdout.
 ;
+;   On entering the function, the contents of RDI, namely the address of the
+;   source string, is pushed on to the stack. It is not moved to the RSI
+;   register where it will be needed eventually because we must first 
+;   calculate the length of the passed-in string, and since RSI is a volatile
+;   register, we have no guarantee that if we place it there strlen will not
+;   modify the contents of RSI.
+;
+;   Alternatively, the contents of RDI could be preserved by using a
+;   non-volatile register like R12-R15, like so:
+;
+;       mov r12, rdi
+;       call strlen
+;       mov rsi, r12
+;
 ; C language equivalent:
 ;
 ;   void puts(const char* str) {
@@ -117,6 +131,44 @@ strlen:
 ;==============================================================================
 
 puts:
+    push rdi            ; Push instruction decrements the stack pointer and
+                        ; stores the source operand on the top of the stack.
+    call strlen
+    pop rsi             ; Loads the value from the top of the stack to the 
+                        ; location specified with the destination operand (or
+                        ; explicit opcode) and then decrements the stack
+                        ; pointer.
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    
+    syscall
+    ret
+
+;==============================================================================
+;
+;                                 PUTCHAR
+;
+;==============================================================================
+;
+; Author: Jose Fernando Lopez Fernandez
+; Date:   11-May-2019
+;
+; Description:
+;
+;   This is a re-implementation of the C Standard Library putchar function,
+;   which prints the passed in argument of type 'char' and prints it to 
+;   stdout.
+;
+; C Language Equivalent:
+;
+;   void putchar(char c) {
+;       ...
+;   }
+;
+;==============================================================================
+
+putchar:
     xor rax, rax
     ret
 
@@ -137,9 +189,28 @@ puts:
 ;   through ld, so _start it is.
 ;
 ;==============================================================================
+;
+; Sample NASM Program
+;
+; _start:
+;     mov rax, 1          ; 'write' syscall number
+;     mov rdi, 1          ; stdout descriptor
+;     mov rsi, msg        ; string address
+;     mov rdx, 14         ; string length in bytes
+;
+;     syscall
+;
+;     mov rax, 60         ; 'exit' syscall number
+;     xor rdi, rdi
+;
+;     syscall
+;
+;==============================================================================
 
 _start:
     mov rdi, str
-    call strlen
+    
+    ; call strlen
+    call puts
 
     call exit
